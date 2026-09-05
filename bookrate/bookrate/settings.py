d1294bc57998de6c9ed3acfd3dd79e2bb29d9995
skam34 <maksymilian.marys@proton.me>
2026-09-05 21:10:45 +0000

    Python anywhere hosting generated files

--- bookrate/bookrate/settings.py
+++ bookrate/bookrate/settings.py
@@ -123,3 +123,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR / 'media')
